c514a8c37f67cdf9cf9a60bfa27c3de32d3f6d78
alu0101028163 <alu0101028163@ull.edu.es>
2019-05-22 00:14:29 +0100

    Fixed major bug that didnt allow page iteration

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,18 +1,30 @@
 from urllib.request import urlopen
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 import sys
+import requests
 import re
+
+
+option = webdriver.ChromeOptions()
+option.add_argument(" — incognito")
+browser = webdriver.Chrome(executable_path='/home/hyydra/Desktop/chromedriver/chromedriver', chrome_options=option)
+
 
 def scrap_category(category_name, search_range):
 
     book_names = []
 
     for i in range(search_range):
-        quote_page = "https://www.goodreads.com/shelf/show/" + category_name + "?page=" + str(i + 1)
-        page = urlopen(quote_page)
-        soup = BeautifulSoup(page,'lxml')
-        names = soup.find_all('a', attrs={"class":'bookTitle'})
 
+        quote_page = "https://www.goodreads.com/shelf/show/" + category_name + "?page=" + str(i + 1)
+        browser.get(quote_page)
+        soup = BeautifulSoup(browser.page_source,'lxml')
+        names = soup.find_all('a', attrs={"class":'bookTitle'})
         for name in names:
                 book_name = name.text
                 book_name = re.sub(r'\"','',book_name)
@@ -44,6 +56,18 @@ if(sys.argv[1] in ('-a','--automatic') ):
     maxpages  = sys.argv[3]
     file_ = open(file_name,'r')
 
+
+    credentials = open("/home/hyydra/Desktop/credentials.txt")
+    user_email = credentials.readline().split()
+    user_password = credentials.readline().split()
+    credentials.close()
+
+    browser.get ("https://www.goodreads.com/user/sign_in")
+
+    browser.find_element_by_id("user_email").send_keys(user_email)
+    browser.find_element_by_id("user_password").send_keys(user_password)
+    browser.find_element_by_name("next").click()
+
     categories = []
     for line in file_:
         categories.append(line.strip())
@@ -51,19 +75,3 @@ if(sys.argv[1] in ('-a','--automatic') ):
     for category in categories:
         book_names = scrap_category(category,int(maxpages))
         category_to_file(("../outputs/" + category + ".txt"), category, book_names)
-        print(book_names)
-
-
-
-#
-# for i in range(2):
-#     quote_page = "https://www.goodreads.com/shelf/show/art?page=" + str(i + 1)
-#     page = urlopen(quote_page)
-#     soup = BeautifulSoup(page,'lxml')
-#     names = soup.find_all('a', attrs={"class":'bookTitle'})
-#
-#     for name in names:
-#             book_names.append(name.text)
-#             # print(name.text)
-#
-# print(len(book_names))
